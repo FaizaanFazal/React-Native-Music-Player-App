f1ade9cdbabeaf1d59d0ab1ad8b06beee0fe44fa
Faizaan <dkfaizaan12@gmail.com>
2023-07-22 08:58:49 -0700

    firstcommit

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import TrackPlayer from 'react-native-track-player';
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
+import {playbackService} from './musicPlayerService'
+
+AppRegistry.registerComponent(appName, () => App);
+TrackPlayer.registerPlaybackService(() => playbackService);
